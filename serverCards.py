08bad819b7094fc8b619802521b45905d86fdb85
Owen Busler <owen1050@gmail.com>
2020-05-30 01:45:17 -0400

    Updates to game, timeoput

--- serverCards.py
+++ serverCards.py
@@ -1,40 +1,82 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import time, threading
 
 port = 23659
+"""
+playerList = ["owen", "ben"]
+pingTime = {"owen" : 0, "ben" : 0}
+hands = {"owen" : "<AS><JD><8H>", "ben" : "<JD><8H>"}
+table = ["AC", "10S"]
+bets = {"owen" : 0, "ben" : 10}
+turn = "owen"
+"""
+
+playerList = []
+pingTime = {}
+hands = {}
+table = []
+bets = {}
+turn = ""
 
 class server(BaseHTTPRequestHandler):
 
-    playerList = []
-    hands = {}
-    table = []
-    bets = {}
-
-
     def do_GET(self):
-        global playersList
+        global playerList, table, bets, hands, turn, pingTime
         content_len = int(self.headers.get('Content-Length'))
-        print(content_len)
         post_body = str(self.rfile.read(content_len))
         body = post_body[2:]
         print(body)
         replyString = ""
         try:
-            
             if(body[0:11] == "~newPlayer:"):
                 i0 = 11
                 i1 = body.find(":", i0)
-                print(i0, i1)
                 newPlayerName = body[i0:i1]
-                print(newPlayerName)
-                
+                playerList.append(newPlayerName)
+                pingTime[newPlayerName] = time.time()
+                hands[newPlayerName]= ""
+                bets[newPlayerName] = 0
                 outString = "New player:" + newPlayerName
-                print(outString)
                 if(replyString == ""):
                     replyString = outString
                 else:
                     replyString = replyString + ":" + outString
         except:
             print("error in new player")
+
+        try:
+            if(body[0:7] == "~update"):
+
+                outString = "~players:"
+                for player in playerList:
+                    outString = outString + str(player) +":"
+                outString = outString + "/table:"
+
+                for card in table:
+                    outString = outString + str(card) +":"
+                outString = outString + "/hands:"
+
+                for playerName in hands:
+                    outString = outString + playerName + "-" + str(hands[playerName]) +":"
+                outString = outString + "/bets:"
+                
+                for playerName in bets:
+                    outString = outString + playerName + "-" + str(bets[playerName]) +":"
+                outString = outString + "/turn:"
+
+                outString = outString + turn + ":/"
+
+                i0 = 7
+                i1 = body.find(":", i0)
+                playerName = body[i0:i1]
+                pingTime[playerName] = time.time()
+
+                if(replyString == ""):
+                    replyString = outString
+                else:
+                    replyString = replyString + ":" + outString
+        except:
+            print("error update")
 
 
         if(replyString == ""):
@@ -45,9 +87,9 @@ class server(BaseHTTPRequestHandler):
         self.send_header('Content-Length',len(replyString))
         self.end_headers()
         self.wfile.write(bytes(replyString, "utf-8"))
-        print("get")
+        print(replyString)
 
-try:
+def startServer():
     # Create a web server and define the handler to manage the
     # incoming request
     ser = HTTPServer(('', port), server)
@@ -55,6 +97,28 @@ try:
 
     # Wait forever for incoming http requests
     ser.serve_forever()
+
+def disconectTest():
+    global playerList, table, bets, hands, turn, pingTime
+    while(True):
+        time.sleep(0.05)
+        try:
+            for player in pingTime:
+                if(pingTime[player] + 5 < time.time()):
+                    playerList.remove(player)
+                    bets.pop(player)
+                    hands.pop(player)
+                    pingTime.pop(player)
+                    print(player + " timed out from the game")
+        except:
+            print("error in test game")
+
+try:
+    serverThread = threading.Thread(target = startServer)
+    serverThread.start()
+
+    dcThread = threading.Thread(target = disconectTest)
+    dcThread.start()
 
 except KeyboardInterrupt:
     print ('^C received, shutting down the web server')
