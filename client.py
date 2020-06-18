db3b995e80e3f991d1f67cff2ad71a7c8e046c88
Owen <Owen1050@gmail.com>
2020-06-17 21:00:52 -0400

    updated server. need tto write out state machine

--- client.py
+++ client.py
@@ -1,10 +1,31 @@
 import requests
 
-url = 'http://192.168.1.254:23659'
+url = 'http://192.168.1.229:23663'
 
-payload = "~newPlayer:ben:~"
+setup = False
+if setup:
+
+    payload = "~update~"
+    headers = {'Content-Length':str(len(payload))}
+
+    r = requests.get(url, data = payload, headers = headers)
+    print(r.text)
+
+    payload = "~!Owen1!~"
+    headers = {'Content-Length':str(len(payload))}
+
+    r = requests.get(url, data = payload, headers = headers)
+    print(r.text)
+
+    payload = "~update~"
+    headers = {'Content-Length':str(len(payload))}
+
+    r = requests.get(url, data = payload, headers = headers)
+    print(r.text)
+
+#payload = "~!Owen2!action:bet:10000:~"
+payload = "~update~"
 headers = {'Content-Length':str(len(payload))}
-
 
 r = requests.get(url, data = payload, headers = headers)
 print(r.text)
--- serverCards.py
+++ serverCards.py
@@ -1,125 +1,90 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time, threading
 
-port = 23659
-"""
-playerList = ["owen", "ben"]
-pingTime = {"owen" : 0, "ben" : 0}
-hands = {"owen" : "<AS><JD><8H>", "ben" : "<JD><8H>"}
-table = ["AC", "10S"]
-bets = {"owen" : 0, "ben" : 10}
-turn = "owen"
-"""
+port = 23663
 
-playerList = []
-pingTime = {}
-hands = {}
-table = []
-bets = {}
-turn = ""
+numberOfPlayers = 0
+
+names = []
+chipCounts = []
+bets = []
+cards = []
 
 class server(BaseHTTPRequestHandler):
+    
+    def log_message(self, format, *args):
+        return
 
     def do_GET(self):
-        global playerList, table, bets, hands, turn, pingTime
+        global numberOfPlayers, names, chipCounts, bets, cards
         content_len = int(self.headers.get('Content-Length'))
         post_body = str(self.rfile.read(content_len))
-        body = post_body[2:]
-        print(body)
+        body = post_body[2:-1]
+        print("body:" + body)
         replyString = ""
-        try:
-            if(body[0:11] == "~newPlayer:"):
-                i0 = 11
+        
+        if body.find("~update") >= 0:
+            replyString = replyString + "~!" + str(numberOfPlayers)+ "!"
+
+            for pi in range(numberOfPlayers):
+                replyString = replyString + "player" + str(pi+1) + ":" +names[pi]+ ":"+ cards[pi][0] + ":" + cards[pi][1] + ":" + str(chipCounts[pi]) + ":" + str(bets[pi]) + ":"
+            replyString = replyString + "~"
+        else:
+            i0 = body.find("!") + 1
+            i1 = body.find("!", i0)
+            playerName = body[i0:i1]
+            print("Name:" + playerName)
+            if playerName in names:
+                pi = names.index(playerName)
+
+                i0 = body.find(":") + 1
                 i1 = body.find(":", i0)
-                newPlayerName = body[i0:i1]
-                playerList.append(newPlayerName)
-                pingTime[newPlayerName] = time.time()
-                hands[newPlayerName]= ""
-                bets[newPlayerName] = 0
-                outString = "New player:" + newPlayerName
-                if(replyString == ""):
-                    replyString = outString
-                else:
-                    replyString = replyString + ":" + outString
-        except:
-            print("error in new player")
 
-        try:
-            if(body[0:7] == "~update"):
+                actionStr = body[i0:i1]
+                print("Action:" + actionStr)
 
-                outString = "~players:"
-                for player in playerList:
-                    outString = outString + str(player) +":"
-                outString = outString + "/table:"
-
-                for card in table:
-                    outString = outString + str(card) +":"
-                outString = outString + "/hands:"
-
-                for playerName in hands:
-                    outString = outString + playerName + "-" + str(hands[playerName]) +":"
-                outString = outString + "/bets:"
-                
-                for playerName in bets:
-                    outString = outString + playerName + "-" + str(bets[playerName]) +":"
-                outString = outString + "/turn:"
-
-                outString = outString + turn + ":/"
-
-                i0 = 7
+                i0 = body.find(":", i1) + 1
                 i1 = body.find(":", i0)
-                playerName = body[i0:i1]
-                pingTime[playerName] = time.time()
 
-                if(replyString == ""):
-                    replyString = outString
-                else:
-                    replyString = replyString + ":" + outString
-        except:
-            print("error update")
+                actionStr2 = body[i0:i1]
+                print("Value:" + actionStr2)
+
+                replyString = "ran action"
+            else:
+                numberOfPlayers = numberOfPlayers + 1
+                names.append(playerName)
+                chipCounts.append(0)
+                bets.append(0)
+                cards.append(["0", "0"])
+                replyString = "Added player " + playerName
+        
+
 
 
         if(replyString == ""):
-            replyString = "Error, no match"
+            replyString = "Error, no action based off body"
 
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.send_header('Content-Length',len(replyString))
         self.end_headers()
         self.wfile.write(bytes(replyString, "utf-8"))
-        print(replyString)
+        print("reply:" + replyString)
+
+ser = HTTPServer(('', port), server)
 
 def startServer():
-    # Create a web server and define the handler to manage the
-    # incoming request
-    ser = HTTPServer(('', port), server)
+    global ser
+    
     print ('Started httpserver on port ' , port)
 
-    # Wait forever for incoming http requests
     ser.serve_forever()
-
-def disconectTest():
-    global playerList, table, bets, hands, turn, pingTime
-    while(True):
-        time.sleep(0.05)
-        try:
-            for player in pingTime:
-                if(pingTime[player] + 5 < time.time()):
-                    playerList.remove(player)
-                    bets.pop(player)
-                    hands.pop(player)
-                    pingTime.pop(player)
-                    print(player + " timed out from the game")
-        except:
-            print("error in test game")
 
 try:
     serverThread = threading.Thread(target = startServer)
     serverThread.start()
 
-    dcThread = threading.Thread(target = disconectTest)
-    dcThread.start()
-
 except KeyboardInterrupt:
     print ('^C received, shutting down the web server')
-    server.socket.close()
+    ser.shutdown()
+    quit()
